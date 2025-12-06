2992b021c7ccfc72faffbb821cf06e00a77c793c
Martino Natali <martino.natali@mail.polimi.it>
2025-12-06 10:15:59 +0100

    "Fix exp_runner.py"

--- exp_runner.py
+++ exp_runner.py
@@ -369,34 +369,34 @@ class Runner:
             writer.write(image)
 
         writer.release()
-    if __name__ == '__main__':
-        print('Hello Wooden')
 
-        torch.set_default_tensor_type('torch.cuda.FloatTensor')
+if __name__ == '__main__':
+    print('Hello Wooden')
 
-        FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
-        logging.basicConfig(level=logging.DEBUG, format=FORMAT)
+    torch.set_default_tensor_type('torch.cuda.FloatTensor')
 
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--conf', type=str, default='./confs/base.conf')
-        parser.add_argument('--mode', type=str, default='train')
-        parser.add_argument('--mcube_threshold', type=float, default=0.0)
-        parser.add_argument('--is_continue', default=False, action="store_true")
-        parser.add_argument('--gpu', type=int, default=0)
-        parser.add_argument('--case', type=str, default='')
+    FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
+    logging.basicConfig(level=logging.DEBUG, format=FORMAT)
 
-        args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--conf', type=str, default='./confs/base.conf')
+    parser.add_argument('--mode', type=str, default='train')
+    parser.add_argument('--mcube_threshold', type=float, default=0.0)
+    parser.add_argument('--is_continue', default=False, action="store_true")
+    parser.add_argument('--gpu', type=int, default=0)
+    parser.add_argument('--case', type=str, default='')
 
-        torch.cuda.set_device(args.gpu)
-        runner = Runner(args.conf, args.mode, args.case, args.is_continue)
+    args = parser.parse_args()
 
-        if args.mode == 'train':
-            runner.train()
-        elif args.mode == 'validate_mesh':
-            runner.validate_mesh(world_space=True, resolution=512, threshold=args.mcube_threshold)
-        elif args.mode.startswith('interpolate'):  # Interpolate views given two image indices
-            _, img_idx_0, img_idx_1 = args.mode.split('_')
-            img_idx_0 = int(img_idx_0)
-            img_idx_1 = int(img_idx_1)
-            runner.interpolate_view(img_idx_0, img_idx_1)
-        
+    torch.cuda.set_device(args.gpu)
+    runner = Runner(args.conf, args.mode, args.case, args.is_continue)
+
+    if args.mode == 'train':
+        runner.train()
+    elif args.mode == 'validate_mesh':
+        runner.validate_mesh(world_space=True, resolution=512, threshold=args.mcube_threshold)
+    elif args.mode.startswith('interpolate'):  # Interpolate views given two image indices
+        _, img_idx_0, img_idx_1 = args.mode.split('_')
+        img_idx_0 = int(img_idx_0)
+        img_idx_1 = int(img_idx_1)
+        runner.interpolate_view(img_idx_0, img_idx_1)
